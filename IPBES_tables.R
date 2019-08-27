ce93fa918d6cee036df6b1b76c96ae4e917de34d
Jaime <jrgarcia.marquez@gmail.com>
2019-08-27 16:25:16 +0200

    read and clean the table

--- IPBES_tables.R
+++ IPBES_tables.R
@@ -2,6 +2,14 @@
 
 ### read the revision table
 
-tb = read.csv(file="sheet1.csv")
+## the table (Sheet General) was downloaded from Google sheets as a .tsv file
 
-### it going to be tricky to analyse this data
+tb = read.table(file="data/general.tsv", header=TRUE, sep="\t", skip=1)
+
+# delete empty rows
+tb = tb[-which(is.na(tb[,1])),]
+
+
+
+tb$ZU
+
